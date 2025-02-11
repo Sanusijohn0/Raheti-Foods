7813314e43bb57a99b45fa5b732ccd2ac8442e48
sanusi john <sanusijohn0@gmail.com>
2025-02-11 09:12:31 +0100

    started menu section

--- JS/index.js
+++ JS/index.js
@@ -15,3 +15,58 @@ navbarTogler.addEventListener("click", () => {
     });
   });
 });
+
+//Menu items
+const foods = [
+  {
+    id: 1,
+    name: "Jollof Rice",
+    price: "6500",
+  },
+  {
+    id: 2,
+    name: "Fried Rice",
+    price: "9000",
+  },
+  {
+    id: 3,
+    name: "Ofada Rice",
+    price: "1000",
+  },
+  {
+    id: 4,
+    name: "Semo",
+    price: "5000",
+  },
+  {
+    id: 5,
+    name: "Amala",
+    price: "7000",
+  },
+];
+
+foods.forEach((food) => {
+  const foodContainer = document.createElement("div");
+  foodContainer.classList.add("food-container");
+
+  const foodName = document.createElement("h4");
+  foodName.classList.add("food-name");
+  foodName.textContent = `${food.name}`;
+  const foodprice = document.createElement("p");
+  foodprice.classList.add("food-price");
+  foodprice.textContent = `Price: N${food.price}`;
+  const quuantity = document.createElement("input");
+  quuantity.setAttribute.type = "number";
+  quuantity.setAttribute.placeholder = "quantity";
+  quuantity.classList.add("quantity");
+  const button = document.createElement("button");
+  button.textContent = "Order";
+  button.classList.add("menu-button");
+
+  foodContainer.appendChild(foodName);
+  foodContainer.appendChild(foodprice);
+  foodContainer.appendChild(quuantity);
+  foodContainer.appendChild(button);
+
+  document.getElementById("menu-container").appendChild(foodContainer);
+});
